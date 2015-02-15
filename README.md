2d622031faf8a3c19b2d0cda939a0b0e227e717b
Eric M <fantasy8.me@gmail.com>
2015-02-15 19:27:41 +0800

    Initial version

---
+++ README.md
@@ -0,0 +1,14 @@
+react-mobile-boilerplate
+=====================
+
+The dev environment to enable live-editing React components.
+
+### Usage
+
+```
+npm install
+npm start
+open http://localhost:3000
+
+```
+Update `app/components/footer/footer.jsx` and see Your changes will appear without reloading the browser
---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval',
+  entry: [
+    'webpack-dev-server/client?http://localhost:3000',
+    'webpack/hot/only-dev-server',
+    './app/app'
+  ],
+  output: {
+    path: __dirname + '/app/',
+    filename: 'bundle.js',
+    publicPath: '/app/'
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoErrorsPlugin()
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ },
+      { test: /\.jsx$/, loaders: ['react-hot', 'jsx?harmony'], exclude: /node_modules/ },
+    ]
+  }
+};
